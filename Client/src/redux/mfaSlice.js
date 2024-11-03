785501d03b7ea76820dac80dade8c3cd6cbcf9b7
sp1deydev <duckhanhthientran@gmail.com>
2024-11-04 02:09:17 +0700

    fix hard code for test

--- Client/src/redux/mfaSlice.js
+++ Client/src/redux/mfaSlice.js
@@ -12,79 +12,61 @@ export const mfaSlice = createSlice({
           numOfImageEachRelationType: 0,
         },
         imageList: [
-          {
-              "uid": "rc-upload-1730085909393-2",
-              "name": "1730085912346-275097726_354306753276397_3570733209895545930_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085912346-275097726_354306753276397_3570733209895545930_n.jpg"
-          },
-          {
-              "uid": "rc-upload-1730085909393-4",
-              "name": "1730085914666-399405994_904004437962999_8837288258346405781_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085914666-399405994_904004437962999_8837288258346405781_n.jpg"
-          },
-          {
-              "uid": "rc-upload-1730085909393-6",
-              "name": "1730085916884-434873873_800172138689218_6175057082809455981_n.png",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085916884-434873873_800172138689218_6175057082809455981_n.png"
-          },
-          {
-              "uid": "rc-upload-1730085909393-8",
-              "name": "1730085921616-438037734_481219580900126_383468623299043374_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085921616-438037734_481219580900126_383468623299043374_n.jpg"
-          },
-          {
-              "uid": "rc-upload-1730085909393-10",
-              "name": "1730085924021-bang diem test 6 ets 2023.png",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085924021-bang diem test 6 ets 2023.png"
-          },
-          {
-              "uid": "rc-upload-1730085909393-12",
-              "name": "1730085926449-451581991_514695577916648_1333109415051119232_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085926449-451581991_514695577916648_1333109415051119232_n.jpg"
-          },
-          {
-              "uid": "rc-upload-1730085909393-14",
-              "name": "1730085930028-434873873_800172138689218_6175057082809455981_n (1).png",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085930028-434873873_800172138689218_6175057082809455981_n (1).png"
-          },
-          {
-              "uid": "rc-upload-1730085909393-16",
-              "name": "1730085932387-436221180_429173306719198_134195243373195111_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085932387-436221180_429173306719198_134195243373195111_n.jpg"
-          },
-          {
-              "uid": "rc-upload-1730085909393-18",
-              "name": "1730085934919-434873873_800172138689218_6175057082809455981_n.png",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085934919-434873873_800172138689218_6175057082809455981_n.png"
-          },
-          {
-              "uid": "rc-upload-1730085909393-20",
-              "name": "1730085939156-438037734_481219580900126_383468623299043374_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085939156-438037734_481219580900126_383468623299043374_n.jpg"
-          },
-          {
-              "uid": "rc-upload-1730085909393-22",
-              "name": "1730085942063-434873873_800172138689218_6175057082809455981_n.png",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085942063-434873873_800172138689218_6175057082809455981_n.png"
-          },
-          {
-              "uid": "rc-upload-1730085909393-26",
-              "name": "1730085956621-368456990_648125967411876_2747958777251265360_n.jpg",
-              "status": "done",
-              "url": "http://localhost:3001/uploads\\1730085956621-368456990_648125967411876_2747958777251265360_n.jpg"
-          }
-      ],
+            {
+                "uid": "rc-upload-1730660552903-3",
+                "name": "1730660596150-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660596150-download.png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-5",
+                "name": "1730660604617-images.jpeg",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660604617-images.jpeg"
+            },
+            {
+                "uid": "rc-upload-1730660552903-7",
+                "name": "1730660606851-download (1).png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660606851-download (1).png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-9",
+                "name": "1730660609580-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660609580-download.png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-11",
+                "name": "1730660611881-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660611881-download.png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-13",
+                "name": "1730660614498-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660614498-download.png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-15",
+                "name": "1730660616673-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660616673-download.png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-17",
+                "name": "1730660618803-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660618803-download.png"
+            },
+            {
+                "uid": "rc-upload-1730660552903-19",
+                "name": "1730660621272-download.png",
+                "status": "done",
+                "url": "http://localhost:3001/uploads\\1730660621272-download.png"
+            }
+        ],
         relationTypes: [
             'Relation Type 1',
             'Relation Type 2',
